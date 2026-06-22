5110785d19f3daa5ca683e43c3b3b5d0e0c88bb4
BazingaLab <mauryaabhinath450@gmail.com>
2026-06-22 13:57:45 +0530

    Add partner section to homepage and navbar

--- server/index.js
+++ server/index.js
@@ -37,5 +37,5 @@ const PORT = process.env.PORT || 5000;
 export default app;
 
 if (process.env.NODE_ENV !== "production") {
-  app.listen(PORT, () => console.log(`✦ Server running on http://localhost:${PORT}`));
+  app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
 }
